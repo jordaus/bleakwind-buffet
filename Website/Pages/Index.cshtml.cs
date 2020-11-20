ce6c6a2c38ddf449cb29e666e8b8ff0af5579c5f
jordaus <jordaus@CIS-RDS2>
2020-11-20 17:00:52 -0600

    finally done get sent to the shadow realm

--- Website/Pages/Index.cshtml.cs
+++ Website/Pages/Index.cshtml.cs
@@ -39,10 +39,68 @@ namespace Website.Pages
 
         public void OnGet(string SearchTerms, string[] FoodType, double? PriceMin, double? PriceMax, int? CalMin, int? CalMax)
         {
-            Order = Menu.Search(SearchTerms);
-            Order = Menu.FilterByFoodType(Order, FoodType);
-            Order = Menu.FilterByCalories(Order, CalMin, CalMax);
-            Order = Menu.FilterByPrice(Order, PriceMin, PriceMax);
+            //Order = Menu.Search(SearchTerms);
+            //Order = Menu.FilterByFoodType(Order, FoodType);
+            //Order = Menu.FilterByCalories(Order, CalMin, CalMax);
+            //Order = Menu.FilterByPrice(Order, PriceMin, PriceMax);
+
+            Order = Menu.FullMenu();
+
+            //Search Terms
+            if(SearchTerms != null)
+            {
+                string temp = SearchTerms;
+                string[] words = temp.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+
+                Order = Order.Where(item =>
+                item.ToString() != null && item.ToString().Contains(SearchTerms, StringComparison.CurrentCultureIgnoreCase) ||
+                item.Description != null && item.Description.Contains(SearchTerms, StringComparison.CurrentCultureIgnoreCase) ||
+                item.Description != null && item.Description.Contains(words[0]) ||
+                item.Description != null && words.Length == 2 && item.Description.Contains(words[1]) ||
+                item.Description != null && words.Length == 3 && item.Description.Contains(words[1]) ||
+                item.Description != null && words.Length == 3 && item.Description.Contains(words[2]));
+            }
+
+            //filter by choice
+            if(FoodType.Length != 0)
+            {
+                Order = Order.Where(item =>
+                item is BleakwindBuffet.Data.Entrees.Entree && FoodType.Contains("Entree") ||
+                item is BleakwindBuffet.Data.Sides.Side && FoodType.Contains("Side") ||
+                item is BleakwindBuffet.Data.Drinks.Drink && FoodType.Contains("Drink"));
+            }
+
+            //Filter by Calories
+            //Max calories
+            if(CalMin == null && CalMax != null)
+            {
+                Order = Order.Where(item => item.Calories <= CalMax);
+            }
+            //Filter for Min Calories
+            if (CalMax == null && CalMin != null)
+            {
+                Order = Order.Where(item => item.Calories >= CalMin);
+            }
+            //Max and Min
+            if (CalMin != null && CalMax != null)
+            {
+                Order = Order.Where(item => 
+                item.Calories >= CalMin && item.Calories <= CalMax);
+            }
+
+            //Price filter
+            if(PriceMin == null && PriceMax != null)
+            {
+                Order = Order.Where(item => item.Price <= PriceMax);
+            }
+            if(PriceMax == null && PriceMin != null)
+            {
+                Order = Order.Where(item => item.Price >= PriceMin);
+            }
+            if(PriceMin != null && PriceMax != null)
+            {
+                Order = Order.Where(item => item.Price >= PriceMin && item.Price <= PriceMax);
+            }
 
             Entree = new List<IOrderItem>();
             Drink = new List<IOrderItem>();
